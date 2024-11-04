fc1d94df2da83d3554a4f5b8fc70655ea29099cf
dohdong <goodstar7381@gmail.com>
2024-11-04 17:55:34 +0900

    first commit

---
+++ src/main/java/com/dohdonglog/DohdonglogApplication.java
@@ -0,0 +1,13 @@
+package com.dohdonglog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DohdonglogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DohdonglogApplication.class, args);
+    }
+
+}
