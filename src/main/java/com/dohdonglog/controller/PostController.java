391ef7839304d17c54e67e44ed8fc49f93644c24
dohdong <goodstar7381@gmail.com>
2024-11-04 18:29:01 +0900

    [#1]feat: post log 찍어봄

--- src/main/java/com/dohdonglog/controller/PostController.java
+++ src/main/java/com/dohdonglog/controller/PostController.java
@@ -1,16 +1,23 @@
 package com.dohdonglog.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class PostController {
 
     //SSR -> jsp, thymeleaf,mustache
     //SPA -> vue , react
 
-    @GetMapping("/posts")
-    public String get() {
+    //HTTP Method
+
+    @PostMapping("/posts")
+    public String post(@RequestParam String title, @RequestParam String content) {
+        log.info("title={}, content={}", title, content);
         return "Hello World";
     }
 
--- src/test/java/com/dohdonglog/controller/PostControllerTest.java
+++ src/test/java/com/dohdonglog/controller/PostControllerTest.java
@@ -1,7 +1,9 @@
 package com.dohdonglog.controller;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -9,6 +11,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(controllers = PostController.class)
@@ -21,9 +24,14 @@ class PostControllerTest {
     @DisplayName("/posts 요청시 Hello World를 출력한다.")
     void test() throws Exception {
         //expected
-        mockMvc.perform(get("/posts"))
+        mockMvc.perform(post("/posts")
+                        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                        .param("title", "글 제목입니다.")
+                        .param("content","글내용입니다. 하하")
+                )
                 .andExpect(status().isOk())
-                .andExpect(content().string("Hello World"));
+                .andExpect(content().string("Hello World"))
+                .andDo(print());
     }
 
 
