122fddd56869145628381b77259652fc011444e6
dohdong <goodstar7381@gmail.com>
2024-11-06 17:54:32 +0900

    [#18]feat: 게시글에 5개씩 페이징 처리

--- src/main/java/com/dohdonglog/service/PostService.java
+++ src/main/java/com/dohdonglog/service/PostService.java
@@ -8,6 +8,8 @@ import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -39,9 +41,17 @@ public class PostService {
 
     }
 
-    public List<PostResponse> getList() {
-        return postRepository.findAll().stream()
+    public List<PostResponse> getList(int page) {
+        Pageable pageable = PageRequest.of(page, 5);
+        return postRepository.findAll(pageable).stream()
                 .map(post -> new PostResponse(post))
                 .collect(Collectors.toList());
     }
+
+
+    // 글이 너무 많은 경우 -> 비용이 많이 든다.
+    // db글 모두 조회하는 경우 -> db가 뻗을수도 있다.
+
+
+
 }
--- src/test/java/com/dohdonglog/service/PostServiceTest.java
+++ src/test/java/com/dohdonglog/service/PostServiceTest.java
@@ -7,6 +7,8 @@ import com.dohdonglog.repository.PostRepository;
 import com.dohdonglog.request.PostCreate;
 import com.dohdonglog.response.PostResponse;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -75,33 +77,27 @@ class PostServiceTest {
     }
 
     @Test
-    @DisplayName("글 여러개 조회")
+    @DisplayName("글 1페이지 조회")
     void test3(){
 
         // given
-        postRepository.saveAll(List.of(
-                Post.builder()
-                        .title("foo1")
-                        .content("bar1")
-                        .build(),
-                Post.builder()
-                        .title("foo2")
-                        .content("bar2")
-                        .build()
-        ));
+        List<Post> requestPosts = IntStream.range(1,31)
+                        .mapToObj(i -> {
+                           return Post.builder()
+                                   .title("호돌맨 제목 " + i)
+                                   .content("반포자이 " + i)
+                                   .build();
+                        })
+                                .collect(Collectors.toList());
+        postRepository.saveAll(requestPosts);
 
-        Post requestPost3 = Post.builder()
-                .title("foo3")
-                .content("bar3")
-                .build();
-        postRepository.save(requestPost3);
 
         // when
-        List<PostResponse> posts = postService.getList();
+        List<PostResponse> posts = postService.getList(0);
 
         // then
-        assertEquals(3L,posts.size());
-
+        assertEquals(5L,posts.size());
+        assertEquals("호돌맨 제목 1",posts.get(0).getTitle());
 
     }
 
