dfc14123a43bdfb4cdcde82b5f3fe38f136067a5
dohdong <goodstar7381@gmail.com>
2024-11-11 16:11:31 +0900

    [#32]feat: 글 등록이 일단 된다!

--- front/src/views/WriteView.vue
+++ front/src/views/WriteView.vue
@@ -1,12 +1,44 @@
 <script setup lang="ts">
 import {ref} from "vue";
+import axios from "axios";
+import {useRouter} from "vue-router";
 
-const count = ref(0);
+const title = ref("");
+const content = ref("");
+
+const router = useRouter();
+
+const write = function () {
+  axios.post("http://localhost:8080/posts",{
+    title: title.value,
+    content: content.value,
+  })
+  // axios
+  // .post("/api/posts", {
+  //   title: title.value,
+  //   content: content.value,
+  // })
+  // .then(() => {
+  //   router.replace({ name: "home" });
+  // });
+};
+
 </script>
 
 <template>
-  <p>안녕하세요</p> <button @click="count += 1">버튼을 눌러주세요</button>
-  <p>{{count}}</p>
+  <div>
+    <el-input v-model="title" placeholder="제목을 입력해주세요" />
+  </div>
+
+  <div class="mt-2">
+    <el-input v-model="content" type="textarea" rows="15" />
+  </div>
+
+  <div class="mt-2">
+    <div class="d-flex justify-content-end">
+      <el-button type="primary" @click="write()">작성완료</el-button>
+    </div>
+  </div>
 </template>
 
 <style scoped>
