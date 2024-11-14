5cebc310bd0f63ae3568af329fecf3eb90c2ea20
dohdong <goodstar7381@gmail.com>
2024-11-14 15:39:40 +0900

    [#36]refactor: Header인증으로 변경

--- src/main/java/com/dohdonglog/config/data/UserSession.java
+++ src/main/java/com/dohdonglog/config/data/UserSession.java
@@ -2,7 +2,9 @@ package com.dohdonglog.config.data;
 
 public class UserSession {
 
-    public Long id;
-    public String name;
+    public final Long id;
 
+    public UserSession(Long id) {
+        this.id = id;
+    }
 }
