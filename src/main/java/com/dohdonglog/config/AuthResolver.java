15fe0fde91773e7d5b3c47f74af645eccc013490
dohdong <goodstar7381@gmail.com>
2024-11-18 12:39:15 +0900

    [#36]feat: 쿠키방식으로 인증

--- src/main/java/com/dohdonglog/config/AuthResolver.java
+++ src/main/java/com/dohdonglog/config/AuthResolver.java
@@ -4,8 +4,11 @@ import com.dohdonglog.config.data.UserSession;
 import com.dohdonglog.domain.Session;
 import com.dohdonglog.exception.Unauthorized;
 import com.dohdonglog.repository.SessionRepository;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.catalina.User;
 import org.springframework.core.MethodParameter;
 import org.springframework.web.bind.support.WebDataBinderFactory;
@@ -13,6 +16,7 @@ import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
+@Slf4j
 @RequiredArgsConstructor
 public class AuthResolver implements HandlerMethodArgumentResolver {
 
@@ -26,10 +30,21 @@ public class AuthResolver implements HandlerMethodArgumentResolver {
     @Override
     public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
                                   NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
-        String accessToken = webRequest.getHeader("Authorization");
-        if(accessToken == null || accessToken.equals("")) {
+//        String accessToken = webRequest.getHeader("Authorization");
+
+        HttpServletRequest servletRequest = webRequest.getNativeRequest(HttpServletRequest.class);
+        if (servletRequest == null) {
+            log.error("servletRequest null");
             throw new Unauthorized();
         }
+
+        Cookie[] cookies = servletRequest.getCookies();
+        if(cookies.length == 0 ) {
+            log.error("쿠키가 없음.");
+            throw new Unauthorized();
+        }
+
+        String accessToken = cookies[0].getValue();
 
         Session session = sessionRepository.findByAccessToken(accessToken)
                 .orElseThrow(() -> new Unauthorized());
--- src/main/java/com/dohdonglog/controller/AuthController.java
+++ src/main/java/com/dohdonglog/controller/AuthController.java
@@ -7,9 +7,13 @@ import com.dohdonglog.repository.UserRepository;
 import com.dohdonglog.request.Login;
 import com.dohdonglog.response.SessionResponse;
 import com.dohdonglog.service.AuthService;
+import java.time.Duration;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,11 +27,27 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/auth/login")
-    public SessionResponse login(@RequestBody Login login) {
+    public ResponseEntity<Object> login(@RequestBody Login login) {
 
         String accessToken = authService.signin(login);
 
-        return new SessionResponse(accessToken);
+//        return new SessionResponse(accessToken);
+
+        ResponseCookie cookie = ResponseCookie.from("SESSION", accessToken)
+                .domain("localhost") // todo 서버 환경에 따른 분리 필요
+                .path("/")
+                .httpOnly(true)
+                .secure(false)
+                .maxAge(Duration.ofDays(30))
+                .sameSite("Strict")
+                .build();
+
+        log.info(">>>> cookie={}", cookie);
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.SET_COOKIE, cookie.toString())
+                .build();
+
 
     }
 
